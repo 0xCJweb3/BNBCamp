b9561b0372e59f51304dc08e63f518b29c044a8a
Jason5680 <jason.zhichao@gmail.com>
2023-04-06 20:38:09 +0800

    feat: upgradeable contracts

---
+++ week4/contracts/StandardProxy/StandardImpl.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.10;
+
+contract StandardImpl {
+    // uint256 public constant VERSION = 1;
+    uint256 public constant VERSION = 2;
+
+    bool public initialized;
+
+    uint256 public value;
+
+    string public name;
+
+    modifier initializer() {
+        require(!initialized, "Only initialize once");
+        _;
+        initialized = true;
+    }
+
+    function initialize(uint256 _initValue, string memory _initName) public initializer {
+        value = _initValue;
+        name = _initName;
+    }
+
+    function setValue(uint256 _newValue) public {
+        value = _newValue + 100;
+    }
+
+    function setName(string memory _newName) public {
+        name = _newName;
+    }
+}
